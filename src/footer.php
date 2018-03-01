0327d8bb81b38f3e71b5b175bb8ce448632f8a91
YULIUS KURNIAWAN KRISTIANTO <yuliuskristianto9@gmail.com>
2018-03-02 05:40:04 +0700

    update jquery

--- src/footer.php
+++ src/footer.php
@@ -5,7 +5,7 @@
     </div>
 </footer>
 
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
         crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/velocity/1.5.1/velocity.min.js"></script>
